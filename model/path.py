2e2b6fe53d162f4fcf93d193657bcdae65bd4a11
Jordy Gelb <jordygelb@gmail.com>
2021-06-10 17:33:19 +0200

    pep8 rules, bug correction, version stable

--- model/path.py
+++ model/path.py
@@ -4,34 +4,36 @@ from typing import List
 from qgis.core import *
 from qgis import processing
 
-
 from .matcheur import Matcheur
 
 
-
 class PathLayer:
-
+    """This class modify directly the selected path layer
+    
+    Parameters:
+    initial_layer: An object of type QgsVectorLayer. It doesn't change through the process
+    layer: An object of type QgsVectorLayer. Represent the path layer modified through the process
+    """
 
     def __init__(self, _layer):
         self.initial_layer = _layer
         self.layer = None
 
-        
     def dupplicate_initial_layer(self):
         """Copy the initial layer and create a 
-           new one with no dependances to the precedent."""
+           new one with no dependances to the precedent.
+        """
 
         try:
             self.initial_layer.selectAll()
             self.layer = processing.run(
-                    "native:saveselectedfeatures", 
-                    {'INPUT': self.initial_layer, 
-                    'OUTPUT': 'memory:'})['OUTPUT']
+                "native:saveselectedfeatures",
+                {'INPUT': self.initial_layer,
+                'OUTPUT': 'memory:'})['OUTPUT']
 
             self.initial_layer.removeSelection()
         except:
             return "path.dupplicate_initial_layer.processing"
-
 
     def create_buffer(self, range: int) -> QgsVectorLayer:
         """Create a buffer around every features of the path_layer.
@@ -47,11 +49,8 @@ class PathLayer:
         feats = [feat for feat in self.layer.getFeatures()]
 
         # Create a buffer around every point of the list
-        buffers = [feat.geometry().buffer(range, -1).asWkt()
-                   for feat in feats]
-
+        buffers = [feat.geometry().buffer(range, -1).asWkt() for feat in feats]
         epsg = self.layer.crs().postgisSrid()
-
         uri = "Polygon?crs=epsg:" + str(epsg) + "&field=id:integer""&index=yes"
 
         # create a new layer
@@ -69,10 +68,8 @@ class PathLayer:
 
         return mem_layer
 
-    #not used
-    def merge_stationary_point(
-            self,speed_column_name: string, 
-            speed_limit: float = 0.1):
+    # not used : can create bugs
+    def merge_stationary_point(self, speed_column_name: string, speed_limit: float = 0.1):
         """Merge at center every group of points 
             which speed is lower than speed_limit.
 
@@ -86,81 +83,77 @@ class PathLayer:
         Modify the layer of this class
         """
 
-        #data validation
+        # data validation
         if speed_limit < 0:
             return "path.negative_speed_limit"
 
         if self.layer.fields().indexFromName(speed_column_name) == -1:
             return "path.wrong_speed_column"
 
-        #We start the process. The layer is ordered, we treat the feature pack by pack
+        # We start the process. The layer is ordered, we treat the feature pack by pack
         start_grouping = False
 
         temporary_feats = []
 
         for feat in self.initial_layer.getFeatures():
 
-            if(feat[speed_column_name] <= speed_limit):
-                #Feature at a stop
+            if feat[speed_column_name] <= speed_limit:
+                # Feature at a stop
                 start_grouping = True
                 temporary_feats.append(feat.id())
 
             else:
-                #Feature mooving
-                if(start_grouping):
-                    #fusion of several points that are at a stop
+                # Feature mooving
+                if start_grouping:
+                    # fusion of several points that are at a stop
                     start_grouping = False
 
                     self.merge_coordinate_points(temporary_feats)
 
-                    #We empty the group and wait for the next one to form
+                    # We empty the group and wait for the next one to form
                     temporary_feats = []
 
-        #Check for the last pack
-        if(start_grouping):
+        # Check for the last pack
+        if start_grouping:
             self.merge_coordinate_points(temporary_feats)
 
-        #Success
+        # Success
         return None
-
     
     def merge_coordinate_points(self,features: List[int]):
         """Merge a list of points at their average center."""
 
-
         average_x = 0
         average_y = 0
-        number_of_iteration =0
+        number_of_iteration = 0
 
-        #We calculate the average position in the group
+        # We calculate the average position in the group
         for feat_id in features:
-            
             f = self.initial_layer.getFeature(feat_id) 
 
             geom = f.geometry()
-            
+
             average_x += geom.asPoint().x()
             average_y += geom.asPoint().y()
 
-            number_of_iteration +=1
+            number_of_iteration += 1
 
         average_x = average_x/number_of_iteration
         average_y = average_y/number_of_iteration
 
-        #We change the position of each point to the calculated average
+        # We change the position of each point to the calculated average
 
         for feat_id in features:
             
             f = self.initial_layer.getFeature(feat_id) 
 
-            #geom = f.geometry()
             geo = QgsGeometry.fromPointXY(QgsPointXY(average_x, average_y))
-            self.layer.dataProvider().changeGeometryValues({ feat_id: geo })
+            self.layer.dataProvider().changeGeometryValues({feat_id: geo})
 
     def speed_point_matching(self,
-                            matcheur: Matcheur, 
-                            speed_column_name: string = "speed", 
-                            speed_limit: float = 1.5):
+                             matcheur: Matcheur,
+                             speed_column_name: string = "speed",
+                             speed_limit: float = 1.5):
         """ Match the point in the path layer to a line by taking into account the speed
         
         Input:
@@ -169,7 +162,7 @@ class PathLayer:
                                in the selected layer
         """
 
-        #Check input validity
+        # Check input validity
         if speed_limit < 0:
             return "path.negative_speed_limit"
 
@@ -178,36 +171,35 @@ class PathLayer:
 
         self.layer.setName("matched point by speed")
 
-        #Start snapping points
+        # Start snapping points
         try:
-            newpts ,error = matcheur.snap_points_along_line(
-                    speedField = speed_column_name, 
-                    speedlim= speed_limit , 
-                    minpts = 5 , 
-                    maxpts = float("inf"))
+            newpts, error = matcheur.snap_points_along_line(
+                speedField=speed_column_name,
+                speedlim= speed_limit,
+                minpts=5,
+                maxpts=float("inf"))
         except:
             return "path.speed_point_matching.matcheur.snap_points_along_line.exception"
 
         if isinstance(newpts, str):
             return "path.speed_point_matching." + newpts
 
-        #Report values to our QgsVectorLayer
+        # Report values to our QgsVectorLayer
         i = 0
         for f in self.layer.getFeatures():
             geo = QgsGeometry.fromPointXY(QgsPointXY(newpts[i].x, newpts[i].y))
-            self.layer.dataProvider().changeGeometryValues({ f.id(): geo })
+            self.layer.dataProvider().changeGeometryValues({f.id(): geo})
             i += 1
 
         if error != []:
-            #Some points have been matched out of the range 
+            # Some points have been matched out of the range 
             print("Warning: " + str(len(error)) + " on " + 
-                str(error[-1]) + " point were matched out of the searching radius ")
+                  str(error[-1]) + " point were matched out of the searching radius ")
             return ("path.speed_point_matching.point_out_of_range-" + 
-                    str(len(error)-1) + "-" + str(error[-1]))
+                    str(len(error) - 1) + "-" + str(error[-1]))
 
-        #Success
+        # Success
         return None
-
 
     def closest_point_matching(self, matcheur: Matcheur):
         """ Match the point in the path layer to the closest point on a line.
@@ -220,19 +212,17 @@ class PathLayer:
         if isinstance(layer, str):
             return "path.closest_point_matching." + layer
 
-
         self.layer = layer
 
         if error != []:
-            #Some points have been matched out of the range 
-            print("Warning: " + str(len(error)) + " on " + 
-                str(error[-1]) + " point were matched out of the searching radius ")
+            # Some points have been matched out of the range 
+            #print("Warning: " + str(len(error)) + " on " + 
+                #str(error[-1]) + " point were matched out of the searching radius ")
             return ("path.closest_point_matching.point_out_of_range-" +
-                    str(len(error)-1) + "-" + str(error[-1]))
+                    str(len(error) - 1) + "-" + str(error[-1]))
 
-        #Succes
+        # Succes
         return None
-
 
     def distance_point_matching(self, matcheur: Matcheur):
         """ Match the point in the path layer to a line 
@@ -250,20 +240,19 @@ class PathLayer:
         self.layer = layer
 
         if error != []:
-            print("Warning: " + str(len(error)) + " on " + str(error[-1]) + 
-                  "point were matched out of the searching radius ")
-            return ("path.distance_point_matching.point_out_of_range-" + 
-                    str(len(error)-1) + "-" + str(error[-1]))
+            #print("Warning: " + str(len(error)) + " on " + str(error[-1]) +
+                  #"point were matched out of the searching radius ")
+            return ("path.distance_point_matching.point_out_of_range-" +
+                    str(len(error) - 1) + "-" + str(error[-1]))
 
-        #Success
+        # Success
         return None
-
 
     def reset_path(self):
         """Reset layer to it's initial value."""
-        
+
         self.initial_layer.selectAll()
-        self.layer = processing.run("native:saveselectedfeatures", 
-                                    {'INPUT': self.initial_layer, 
-                                    'OUTPUT': 'memory:'})['OUTPUT']
+        self.layer = processing.run("native:saveselectedfeatures",
+                                    {'INPUT': self.initial_layer,
+                                     'OUTPUT': 'memory:'})['OUTPUT']
         self.initial_layer.removeSelection()
--- model/ui/layer_manager.py
+++ model/ui/layer_manager.py
@@ -5,6 +5,9 @@ import string
 
 
 class LayerManager:
+
+    """This class manage every layers present on QGIS."""
+
     path_codes = [1001]
     network_codes = [1002]
 
@@ -15,59 +18,49 @@ class LayerManager:
         self.OID = None
         self.speed = None
 
-
     def set_layers(self, layers: List[QgsVectorLayer]) -> None:
         self.layers = layers
-
 
     def add_layer(self, layer: QgsVectorLayer) -> None: #modif
         self.layers.append(layer)
         QgsProject.instance().addMapLayer(layer)
 
-
     def remove_layer(self, _from: int) -> None:
         try:
             self.layers.pop(_from)
         except:
-            #index out of range
-            return 
+            # index out of range
+            return
 
-
-    def remove_layer_from_name(self, name: string) ->None:
+    def remove_layer_from_name(self, name: string) -> None:
         """Delete a layer on Qgis and in the programm that has the same name"""
         try:
             for layer in self.layers:
-                if(layer.sourceName() == name):
+                if (layer.sourceName() == name):
                     QgsProject.instance().removeMapLayer(self.find_layer(name))
                     self.layers.remove(layer)
                     return
         except Exception:
-            #print("Couldn't delete this vector")
+            # print("Couldn't delete this vector")
             return
-            
 
-        #print("Didn't found the layer in the list")
+        # print("Didn't found the layer in the list")
             
-
-    def deselect_layer(self, name: string) ->None: #modif
+    def deselect_layer(self, name: string) ->None:
         QgsProject.instance().layerTreeRoot().findLayer(self.find_layer(name).id()).setItemVisibilityChecked(False)
-
 
     def get_path_layers(self) -> List[QgsVectorLayer]:
         return [layer for layer in self.layers if LayerManager.is_path_layer(layer)]
 
-
     def get_matched_layers(self) -> List[QgsVectorLayer]:
 
-        return [layer for layer in self.layers if   LayerManager.is_path_layer(layer) and 
-                                                    (layer.sourceName().split('_')[0] == "matched point by distance" or
-                                                    layer.sourceName().split('_')[0] == "matched point by speed" or
-                                                    layer.sourceName().split('_')[0] == "matched point to closest")]
-
+        return [layer for layer in self.layers if LayerManager.is_path_layer(layer) and 
+                (layer.sourceName().split('_')[0] == "matched point by distance" or
+                 layer.sourceName().split('_')[0] == "matched point by speed" or
+                 layer.sourceName().split('_')[0] == "matched point to closest")]
 
     def get_network_layers(self) -> List[QgsVectorLayer]:
         return [layer for layer in self.layers if LayerManager.is_network_layer(layer)]
-
 
     def get_path_attributes(self, index: int) -> List[QgsField]:
         try:
@@ -75,14 +68,11 @@ class LayerManager:
         except Exception:
             return []
 
-    
     def load_layer(self, path: str) -> None:
         layer = QgsVectorLayer(path, os.path.basename(path), "ogr")
         self.layers.append(layer)
 
-
     def find_layer(self, name: string) -> QgsVectorLayer:
-
         try:
             for layer in self.layers:
                 if layer.name() == name:
@@ -91,7 +81,7 @@ class LayerManager:
             print("Error while searching for the layer " + name)
         return None
 
-    def save(self, path: string, network : string, OID : string, speed : string) -> None:
+    def save(self, path: string, network: string, OID: string, speed: string) -> None:
         """Store values for later restoration or uses"""
 
         self.selected_path = path
@@ -99,33 +89,27 @@ class LayerManager:
         self.OID = OID
         self.speed = speed
 
-
-
     @classmethod
     def is_path_layer(cls, layer: QgsVectorLayer) -> bool:
         #print(layer.sourceName())
         return QgsWkbTypes.flatType(layer.wkbType()) == QgsWkbTypes.Point
 
-
     @classmethod
     def is_network_layer(cls, layer: QgsVectorLayer) -> bool:
         return QgsWkbTypes.flatType(layer.wkbType()) == QgsWkbTypes.LineString
-
 
     @classmethod
     def is_valid(cls, layer: QgsVectorLayer) -> bool:
         return (cls.is_path_layer(layer) or cls.is_network_layer) and layer.isValid()
 
-
     @classmethod
-    def are_valid(cls, path: QgsVectorLayer, network: QgsVectorLayer) ->bool:
-        
+    def are_valid(cls, path: QgsVectorLayer, network: QgsVectorLayer) -> bool:
+
         if not (cls.is_valid(path) and cls.is_valid(network)):
             return False
-        if network.crs().mapUnits() != 0 or path.crs().mapUnits() != 0  :
+        if network.crs().mapUnits() != 0 or path.crs().mapUnits() != 0:
             return False
-        if network.crs().authid() != path.crs().authid() :
+        if network.crs().authid() != path.crs().authid():
             return False
 
         return True
-        
